d2536b5b1fcedbfc8233ee4905289c1f11ebe277
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-30 08:19:31 -0400

    added palindrome algo(s)

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+# O(n) Time | O(n) Space
 def isPalindrome(string):
     """
     Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a
     palindrome.
     """
-    pass
+    if string[::-1] == string:
+        return True
+    return False
+
+
+# O(n) Time | O(1) space
+# Takes advantage of the fact that's letter is equal to respective last letter, like a mirror reflection.
+def isPalindromeBetter(string):
+    start = 0
+    end = len(string) - 1
+    while start < end:
+        if string[start] != string[end]:
+            return False
+        else:
+            start += 1
+            end -= 1
+    return True
