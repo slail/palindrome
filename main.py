d4d435dc86ef65b30534d39c95ed3a06b72336bf
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-30 08:12:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def isPalindrome(string):
+    """
+    Write a function that takes in a non-empty string and that returns a boolean representing whether the string is a
+    palindrome.
+    """
+    pass
